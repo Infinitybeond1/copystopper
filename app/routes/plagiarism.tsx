eb199508d4a092067d55f00fe0f685e06919afbc
Luke <Infinitybeond1@protonmail.com>
2022-11-19 19:34:08 -0500

    finsih styling

--- app/routes/plagiarism.tsx
+++ app/routes/plagiarism.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "@remix-run/react";
 export default function Plagiarism() {
   return (
-    <>
-      <h1>Plagiarism Checker</h1>
-      <Outlet />
-    </>
+    <div className="grid h-screen place-items-center bg-white dark:bg-black">
+      <div className="p-5 rounded-3xl text-lg bg-accent-light text-black dark:bg-accent-dark dark:text-white">
+        <h1>Plagiarism Checker</h1>
+        <Outlet />
+      </div>
+    </div>
   );
 }
